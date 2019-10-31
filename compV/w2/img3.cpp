6a4153054f692a5b75ced9eb188eef2e8ab23fe5
James Lim Sarrias <jl17056@it075675.wks.bris.ac.uk>
2019-10-31 17:16:59 +0000

    32

--- compV/w2/img3.cpp
+++ compV/w2/img3.cpp
@@ -11,10 +11,10 @@ int main() {
   // Read image from file
   Mat image = imread("mandrill1.jpg", 1);
   Mat image2 = imread("mandrill1.jpg", 1);
-  
+
   for(int y=0;y<image.rows;y++){
     for(int x =0;x<image.cols;x++){
-      image2.at<Vec3b>((y+30)%image.rows,(x+30)%image.cols)[2] = image.at<Vec3b>(y,x)[2];
+      image2.at<Vec3b>((y+32)%image.rows,(x+32)%image.cols)[2] = image.at<Vec3b>(y,x)[2];
     }
   }
   imwrite("corrected3.jpg", image2);
