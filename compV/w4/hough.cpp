665d7404e5d54d8ce5fad7eee44186b583f1ea40
Tharidu Wicramasinghe Weerakkodige <tj17908@it075640.wks.bris.ac.uk>
2019-11-12 12:40:41 +0000

    noSeg

--- compV/w4/hough.cpp
+++ compV/w4/hough.cpp
@@ -5,49 +5,53 @@
 
 using namespace cv;
 
-Mat sobel()
-{
-  Mat input = imread("coin1.png",0);
-  Mat output1(input.rows,input.cols, CV_32FC1, Scalar(0));
-  Mat output2(input.rows,input.cols, CV_32FC1, Scalar(0));
+Mat sobel(Mat input) {
+
+  Mat magnitude(input.rows,input.cols, CV_32FC1, Scalar(0));
+  Mat direction(input.rows,input.cols, CV_32FC1, Scalar(0));
   Mat kernelx = (Mat_<int>(3,3) << -1,0,1,-2,0,2,-1,0,1);
   Mat kernely = kernelx.t();
 
- for(int y=1;y<input.rows-1;y++){
-    for(int x =1;x<input.cols-1;x++){
+ for(int y=1;y<input.rows-1;y++) {
+    for(int x =1;x<input.cols-1;x++) {
        float pixelx = 0;
-     float pixely = 0;
-      for(int i=-1;i<2;i++){
+       float pixely = 0;
+       for(int i=-1;i<2;i++){
           for(int j=-1;j<2;j++){
               pixelx +=  input.at<uchar>(y+i,x+j) * kernelx.at<int>(i+1,j+1);
-             pixely +=  input.at<uchar>(y+i,x+j) * kernely.at<int>(i+1,j+1);
+              pixely +=  input.at<uchar>(y+i,x+j) * kernely.at<int>(i+1,j+1);
           }
+        }
+        magnitude.at<float>(y, x) = sqrt((pixelx*pixelx)+(pixely*pixely));
+        direction.at<float>(y, x) = atan2(pixely,pixelx);
+     }
+   }
+   Mat unnorm = direction;
+   cv::normalize(magnitude,magnitude,0,255,NORM_MINMAX);
+   cv::normalize(direction,direction,0,255,NORM_MINMAX);
 
+
+   imwrite("magnitude.jpg", magnitude);
+   imwrite("direction.jpg", direction);
+   return unnorm;
+}
+
+void threshold(Mat input, int val){
+
+  Mat result(input.rows, input.cols, CV_8UC1, Scalar(0));
+  for(int x=0; x<input.rows; x++) {
+    for(int y=0; y<input.cols; y++) {
+      if (input.at<uchar>(x,y)>val) {
+        result.at<uchar>(x,y)=255;
       }
-     output1.at<float>(y, x) = sqrt((pixelx*pixelx)+(pixely*pixely));
-     output2.at<float>(y, x) = atan2(pixely,pixelx);
-     }
-   }
-   cv::normalize(output1,output1,0,255,NORM_MINMAX);
-   return output2;
+      else {
+        result.at<uchar>(x,y)=0;
+      }
+    }
+  }
+  imwrite("thresholded.jpg", result);
 }
 
-
-Mat _threshold(int val){
-  Mat image = imread("magnitude.jpg",0);
-  Mat result(image.rows, image.cols, CV_8UC1,Scalar(0));
-  for(int x=0; x<image.rows; x++) {
-   for(int y=0; y<image.cols; y++) {
-     if (image.at<uchar>(x,y)>val) {
-       result.at<uchar>(x,y)=255;
-     }
-     else {
-       result.at<uchar>(x,y)=0;
-     }
-   }
- }
- return result;
-}
 int ***malloc3dArray(int dim1, int dim2, int dim3)
 {
     int i, j, k;
@@ -63,50 +67,58 @@ int ***malloc3dArray(int dim1, int dim2, int dim3)
     return array;
 }
 
-void hough(){
-  Mat thresholded = _threshold(65);
-  Mat dir = sobel();
+void hough(Mat thresholded, Mat dir) {
   int ***accumulator = malloc3dArray(thresholded.rows,thresholded.cols,100);
-  for(int i = 0;i<thresholded.rows;i++){
-    for(int j = 0;j<thresholded.cols;j++){
+  for(int i = 0;i<thresholded.rows;i++) {
+    for(int j = 0;j<thresholded.cols;j++) {
       for(int r = 0;r<100;r++){
       accumulator[i][j][r] = 0;
      }
     }
   }
-  for(int y=0;y<thresholded.rows;y++){
-    for(int x =0;x<thresholded.cols;x++){
-      for(int r =20;r<100;r++){
+
+  for(int y=0;y<thresholded.rows;y++) {
+    for(int x =0;x<thresholded.cols;x++) {
+      for(int r =20;r<=100;r++){
         if(thresholded.at<uchar>(y,x)==255){
             int yo = y + r*std::sin(dir.at<float>(y,x));
             int xo = x + r*std::cos(dir.at<float>(y,x));
-          if(xo>=0 &&yo>=0 && yo<thresholded.rows && xo<thresholded.cols){
-            accumulator[yo][xo][r] += 1;
+            if(xo>=0 &&yo>=0 && yo<thresholded.rows && xo<thresholded.cols){
+              accumulator[yo][xo][r] += 1;
+            }
+            int xon = x - r*std::cos(dir.at<float>(y,x));
+            int yon = y - r*std::sin(dir.at<float>(y,x));
+            if(xon>=0 &&yon>=0 && xon<thresholded.cols && yon<thresholded.rows){
+              accumulator[yon][xon][r] += 1;
+            }
           }
-          int xon = x - r*std::cos(dir.at<float>(y,x));
-          int yon = y - r*std::sin(dir.at<float>(y,x));
-        if(xon>=0 &&yon>=0 && xon<thresholded.cols && yon<thresholded.rows){
-          accumulator[yon][xon][r] += 1;
         }
       }
     }
-  }
-}
-  Mat convToMat(thresholded.rows,thresholded.cols, CV_32FC1, Scalar(0));
-  for(int i=0;i<thresholded.rows;i++){
-    for(int j=0;j<thresholded.cols;j++){
-      for(int r=0;r<100;r++){
-        convToMat.at<float>(i,j) = accumulator[i][j][r];
+
+    Mat convToMat(thresholded.rows,thresholded.cols, CV_32FC1, Scalar(0));
+    for(int i=0;i<thresholded.rows;i++){
+      for(int j=0;j<thresholded.cols;j++){
+          convToMat.at<float>(i,j) = 0;
+        for(int r=0;r<100;r++){
+          convToMat.at<float>(i,j) += accumulator[i][j][r];
+        }
       }
     }
-  }
-  imwrite("houghspace.jpg",convToMat);
+    cv::normalize(convToMat,convToMat,0,255,NORM_MINMAX);
+    imwrite("houghspace.jpg",convToMat);
 }
 
 int main(int argc, char** argv) {
-  // char* imageName = argv[1];
-  // Mat image = imread(imageName,1);
-  // imwrite("thresholded.jpg",threshold(65));
-  hough();
+  Mat input = imread("coins1.png", 0);
+  Mat unnorm = sobel(input);
+
+  Mat direction = imread("direction.jpg", 0);
+  Mat magnitude = imread("magnitude.jpg", 0);
+  threshold(magnitude, 65);
+
+  Mat thresholded = imread("thresholded.jpg", 0);
+  hough(thresholded, unnorm);
+
   return 0;
 }
