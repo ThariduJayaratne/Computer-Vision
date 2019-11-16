4e55d8de66ed185d148c52bccfc620c977a30c58
Tharidu Wicramasinghe Weerakkodige <tj17908@it075650.wks.bris.ac.uk>
2019-11-16 12:09:41 +0000

    2a

--- compV/materials/face.cpp
+++ compV/materials/face.cpp
@@ -1,10 +1,4 @@
-/////////////////////////////////////////////////////////////////////////////
-//
-// COMS30121 - face.cpp
-//
-/////////////////////////////////////////////////////////////////////////////
 
-// header inclusion
 #include <stdio.h>
 #include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/opencv.hpp"
@@ -16,29 +10,6 @@
 
 using namespace std;
 using namespace cv;
-
-Mat img4 = (Mat_<int>(1,4) << 362, 128, 460, 243);
-
-Mat img5 = (Mat_<int>(11,4) << 67,147,118,192,
-															 59,258,111,311,
-														 	 200,226,249,276,
-															 255,172,301,222,
-															 299,244,342,298,
-															 439,241,480,296,
-															 384,195,434,240,
-															 518,186,565,232,
-															 566,249,613,308,
-															 682,255,726,309,
-															 651,192,697,237);
-
-Mat img13 = (Mat_<int>(1,4) << 427,123,521,244);
-
-Mat img14 = (Mat_<int>(2,4) << 732,196,825,285,
-															 474,217,550,315);
-
-Mat img15 = (Mat_<int>(3,4) << 73,132,131,214,
-															 370,114,429,192,
-															 539,131,608,216);
 
 /** Function Headers */
 void detectAndDisplay( Mat frame );
@@ -59,26 +30,43 @@ int main( int argc, const char** argv )
 	// 3. Detect Faces and Display Result
 	detectAndDisplay( frame );
 
-	// for (int i = 0; i < img15.rows; i++) {
-	// 	rectangle(frame, Point(img15.at<int>(i,0), img15.at<int>(i,1)), Point(img15.at<int>(i,2), img15.at<int>(i,3)), Scalar(0, 0, 255), 2);
-	// }
-	// rectangle(frame, Point(362, 128), Point(460, 243), Scalar(0, 0, 255), 2);
-	// 4. Save Result Image
-	// imwrite( "dart15T.jpg", frame );
-
-// int x,y = widthAndHeight(img5);
-// printf("%f",x);
-
 	return 0;
 }
 
 /** @function detectAndDisplay */
 void detectAndDisplay( Mat frame )
 {
+	std::vector<Rect> img4;
+	img4.push_back(Rect(350,101,127,159));
+
+	std::vector<Rect> img5;
+	img5.push_back(Rect(67,147,51,45));
+	img5.push_back(Rect(59,258,52,53));
+	img5.push_back(Rect(200,226,49,50));
+	img5.push_back(Rect(255,172,46,50));
+	img5.push_back(Rect(299,244,43,54));
+	img5.push_back(Rect(439,241,41,55));
+	img5.push_back(Rect(384,195,50,45));
+	img5.push_back(Rect(518,186,47,46));
+	img5.push_back(Rect(566,249,47,59));
+	img5.push_back(Rect(682,255,44,54));
+	img5.push_back(Rect(651,192,46,45));
+
+	std::vector<Rect> img13;
+	img13.push_back(Rect(427,123,94,121));
+
+	std::vector<Rect> img14;
+	img14.push_back(Rect(732,196,83,89));
+	img14.push_back(Rect(474,217,76,98));
+
+	std::vector<Rect> img15;
+	img15.push_back(Rect(73,132,58,82));
+	img15.push_back(Rect(370,114,59,78));
+	img15.push_back(Rect(539,131,69,85));
+
 	std::vector<Rect> faces;
 	Mat frame_gray;
 	int width,height;
-
 
 	// 1. Prepare Image by turning it into Grayscale and normalising lighting
 	cvtColor( frame, frame_gray, CV_BGR2GRAY );
@@ -94,15 +82,44 @@ void detectAndDisplay( Mat frame )
 	for( int i = 0; i < faces.size(); i++ )
 	{
 		rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
-		printf("Area: %d\n", faces[i].width * faces[i].height);
 	}
-		//Draw box around true Faces
-		for (int i = 0; i < img4.rows; i++) {
-			rectangle(frame, Point(img4.at<int>(i,0), img4.at<int>(i,1)), Point(img4.at<int>(i,2), img4.at<int>(i,3)), Scalar(0, 0, 255), 2);
-			width = (img4.at<int>(i,2)) - (img4.at<int>(i,0));
-			height = (img4.at<int>(i,3)) - (img4.at<int>(i,1));
-			int area = width * height;
-			printf("Area of truth: %d\n", area);
+	float truePositives = 0;
+
+	for( int i = 0; i < img15.size(); i++ )
+	{
+		rectangle(frame, Point(img15[i].x, img15[i].y), Point(img15[i].x + img15[i].width, img15[i].y + img15[i].height), Scalar( 0, 0, 255 ), 2);
+		truePositives += 1;
+	}
+
+	float falsePositives = 0;
+	for (int i = 0; i < faces.size(); i++) {
+		for (int j = 0; j < img15.size(); j++) {
+			Rect r1(faces[i].x, faces[i].y,faces[i].width,faces[i].height);
+			Rect r2(img15[j].x, img15[j].y,img15[j].width,img15[j].height);
+			Rect intersection = r1 & r2;
+			Rect unionA = r1 | r2;
+			float iWidth = intersection.width;
+			float iHeight = intersection.height;
+			float uWidth = unionA.width;
+			float uHeight = unionA.height;
+			float areaRatio = (iWidth * iHeight) / (uWidth * uHeight);
+			if (areaRatio > 0.2f) {
+				 printf("IOU is %f\n", i, j, areaRatio);
+				 // printf("intersection is %d union is %d\n",intersection.area(),unionA.area());
+			 }
+			 else if(areaRatio > 0 && areaRatio < 0.2){
+				 falsePositives++;
+			 }
 		}
+	}
+	float tpr = truePositives/faces.size();
+	float precision = truePositives/(truePositives+falsePositives);
+	float f1score = (2*(tpr*precision))/(tpr+precision);
+	printf("True positives: %f\n", truePositives);
+	printf("False positives: %f\n", falsePositives);
+	printf("Tpr: %f\n", tpr);
+	printf("F1 score is: %f\n", f1score);
+
+	imwrite("dart15T.jpg", frame);
 
 }
