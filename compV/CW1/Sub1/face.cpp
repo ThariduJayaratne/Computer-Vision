9e84d0d72deb96e55733295cb00f00a90a80b38e
James Lim Sarrias <jl17056@it075854.wks.bris.ac.uk>
2019-11-18 14:50:11 +0000

    fixed folders

---
+++ compV/CW1/Sub1/face.cpp
@@ -0,0 +1,126 @@
+
+#include <stdio.h>
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/opencv.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+using namespace cv;
+
+/** Function Headers */
+void detectAndDisplay( Mat frame );
+
+/** Global variables */
+String cascade_name = "frontalface.xml";
+CascadeClassifier cascade;
+
+/** @function main */
+int main( int argc, const char** argv )
+{
+       // 1. Read Input Image
+	Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
+	// 2. Load thfile:///usr/share/doc/HTML/index.htmle Strong Classifier in a structure called `Cascade'
+	if( !cascade.load( cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+
+	// 3. Detect Faces and Display Result
+	detectAndDisplay( frame );
+
+	return 0;
+}
+
+/** @function detectAndDisplay */
+void detectAndDisplay( Mat frame )
+{
+	std::vector<Rect> img4;
+	img4.push_back(Rect(350,101,127,159));
+
+	std::vector<Rect> img5;
+	img5.push_back(Rect(67,147,51,45));
+	img5.push_back(Rect(59,258,52,53));
+	img5.push_back(Rect(200,226,49,50));
+	img5.push_back(Rect(255,172,46,50));
+	img5.push_back(Rect(299,244,43,54));
+	img5.push_back(Rect(439,241,41,55));
+	img5.push_back(Rect(384,195,50,45));
+	img5.push_back(Rect(518,186,47,46));
+	img5.push_back(Rect(566,249,47,59));
+	img5.push_back(Rect(682,255,44,54));
+	img5.push_back(Rect(651,192,46,45));
+
+	std::vector<Rect> img13;
+	img13.push_back(Rect(427,123,94,121));
+
+	std::vector<Rect> img14;
+	img14.push_back(Rect(732,196,83,89));
+	img14.push_back(Rect(474,217,76,98));
+
+	std::vector<Rect> img15;
+	img15.push_back(Rect(73,132,58,82));
+	img15.push_back(Rect(370,114,59,78));
+	img15.push_back(Rect(539,131,69,85));
+
+	std::vector<Rect> faces;
+	Mat frame_gray;
+	int width,height;
+
+	// 1. Prepare Image by turning it into Grayscale and normalising lighting
+	cvtColor( frame, frame_gray, CV_BGR2GRAY );
+	equalizeHist( frame_gray, frame_gray );
+
+	// 2. Perform Viola-Jones Object Detection
+	cascade.detectMultiScale( frame_gray, faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
+
+       // 3. Print number of Faces found
+	std::cout << faces.size() << std::endl;
+
+       // 4. Draw box around faces found
+	for( int i = 0; i < faces.size(); i++ )
+	{
+		rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
+	}
+	float truePositives = 0;
+
+	for( int i = 0; i < img5.size(); i++ )
+	{
+		rectangle(frame, Point(img5[i].x, img5[i].y), Point(img5[i].x + img5[i].width, img5[i].y + img5[i].height), Scalar( 0, 0, 255 ), 2);
+	}
+
+	float falsePositives = 0;
+	for (int i = 0; i < faces.size(); i++) {
+		int correct = 0;
+		for (int j = 0; j < img5.size(); j++) {
+			Rect r1(faces[i].x, faces[i].y,faces[i].width,faces[i].height);
+			Rect r2(img5[j].x, img5[j].y,img5[j].width,img5[j].height);
+			Rect intersection = r1 & r2;
+			Rect unionA = r1 | r2;
+			float iWidth = intersection.width;
+			float iHeight = intersection.height;
+			float uWidth = unionA.width;
+			float uHeight = unionA.height;
+			float areaRatio = (iWidth * iHeight) / (uWidth * uHeight);
+			if (areaRatio > 0.2f) {
+				correct = 1;
+				truePositives += 1;
+				printf("IOU is %f\n", i, j, areaRatio);
+			 }
+		}
+		if (correct == 0) {
+			falsePositives++;
+		}
+	}
+	float tpr = truePositives/faces.size();
+	float precision = truePositives/(truePositives+falsePositives);
+	float f1score = (2*(tpr*precision))/(tpr+precision);
+	printf("True positives: %f\n", truePositives);
+	printf("False positives: %f\n", falsePositives);
+	printf("Tpr: %f\n", tpr);
+	printf("F1 score is: %f\n", f1score);
+
+	imwrite("dart5T.jpg", frame);
+
+}
