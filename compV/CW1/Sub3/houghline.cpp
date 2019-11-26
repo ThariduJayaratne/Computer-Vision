2037dc8149192fc768d2fc6e96323dd2047175f2
Tharidu Wicramasinghe Weerakkodige <tj17908@it075650.wks.bris.ac.uk>
2019-11-26 19:35:23 +0000

    gridsearch

--- compV/CW1/Sub3/houghline.cpp
+++ compV/CW1/Sub3/houghline.cpp
@@ -73,42 +73,37 @@ void threshold(Mat input, int val) {
   imwrite("threshold.jpg", output);
 }
 
-void houghline(Mat threshold, Mat direction){
-  int diagonal = sqrt((threshold.rows*threshold.rows)+(threshold.cols*threshold.cols));
-  Mat output(threshold.rows, threshold.cols, CV_32FC1, Scalar(0));
-  int accumulator[180][diagonal];
-  for (int i = 0; i < diagonal; i++) {
-    for (int j = 0; j < 180 ; j++) {
-        accumulator[j][i] = 0;
-      }
-    }
-    for (int x = 0; x < threshold.rows; x++) {
-      for (int y = 0; y < threshold.cols; y++) {
-          float angle = (direction.at<float>(x,y)*180)/3.1415926;
-          int variation = 5;
-          if (threshold.at<uchar>(x, y) == 255) {
-            float minA,maxA =0;
-            if (angle<0) minA += 180+variation;
-            else if(angle>180) maxA -= 180+variation;
-            for(int z =0;z<180;z++){
-              if(angle>minA && angle<maxA){
-              float perpenD = x*cos(z)+ y*sin(z);
-              if(perpenD<=diagonal){
-                accumulator[z][(int)perpenD] +=1;
-              }
-            }
-          }
-        }
-      }
-    }
-    for(int i=0;i<diagonal;i++){
-      for(int j=0;j<180;j++){
-        if(accumulator[j][i] > 255) accumulator[j][i] = 255;
-        output.at<uchar>(j,i) = accumulator[j][i];
-      }
-    }
-    imwrite("dart3lines.jpg", output);
-}
+// void houghline(Mat threshold, Mat direction){
+//   int diagonal = sqrt((threshold.rows*threshold.rows)+(threshold.cols*threshold.cols));
+//   Mat output(threshold.rows, threshold.cols, CV_32FC1, Scalar(0));
+//   int accumulator[180][diagonal];
+//   for (int i = 0; i < diagonal; i++) {
+//     for (int j = 0; j < 180 ; j++) {
+//         accumulator[j][i] = 0;
+//       }
+//     }
+//     for (int x = 0; x < threshold.rows; x++) {
+//       for (int y = 0; y < threshold.cols; y++) {
+//           float angle = (direction.at<float>(x,y)*180)/3.1415926;
+//           int variation = 5;
+//           if (threshold.at<uchar>(x, y) == 255) {
+//             float minA,maxA =0;
+//             if (angle<0) minA += 180+variation;
+//             else if(angle>180) maxA -= 180+variation;
+//             for(int z =0;z<180;z++){
+//               if(angle>minA && angle<maxA){
+//               float perpenD = x*cos(z)+ y*sin(z);
+//               if(perpenD<=diagonal){
+//                 accumulator[z][(int)perpenD] +=1;
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//     normalize(output, output, 0, 255, NORM_MINMAX);
+//     imwrite("dart3lines.jpg", output);
+// }
 
 
 int ***malloc3dArray(int dim1, int dim2, int dim3)
@@ -127,7 +122,6 @@ int ***malloc3dArray(int dim1, int dim2, int dim3)
 vector<Rect> hough(Mat threshold, Mat direction, int radius) {
 
   Mat output(threshold.rows, threshold.cols, CV_32FC1, Scalar(0));
-  // int accumulator[threshold.rows][threshold.cols][100];
   int ***accumulator = malloc3dArray(threshold.rows,threshold.cols,radius);
   for (int i = 0; i < threshold.rows; i++) {
     for (int j = 0; j < threshold.cols; j++) {
@@ -139,7 +133,7 @@ vector<Rect> hough(Mat threshold, Mat direction, int radius) {
 
   for (int x = 0; x < threshold.rows; x++) {
     for (int y = 0; y < threshold.cols; y++) {
-      if (threshold.at<uchar>(x, y) == 255) {
+      if (threshold.at<uchar>(x, y) >= 255) {
         for (int r = 5; r <= radius; r++) {
 
           int xo = x + r * sin(direction.at<float>(x, y));
@@ -164,19 +158,33 @@ vector<Rect> hough(Mat threshold, Mat direction, int radius) {
 
   normalize(output, output, 0, 255, NORM_MINMAX);
   std::vector<Rect> img;
-  for (int i = 0; i < output.rows; i++) {
-    for (int j = 0; j < output.cols; j++) {
-      if (output.at<float>(i, j) == 255) {
-        int maxRadius = 0;
-        int temp = 0;
-        for (int r = 0; r < radius; r++) {
-          if (accumulator[i][j][r] > temp) {
-            temp = accumulator[i][j][r];
-            maxRadius = r;
+  int xdivisions = output.rows/4;
+  int ydivisions = output.cols/4;
+  for (int x = 0; x < xdivisions; x++) {
+    for (int y = 0; y < ydivisions; y++) {
+      int found = 0;
+      int completed = 0;
+      while (found == 0 && completed == 0) {
+        for (int i = x * xdivisions; (i < (x + 1) * xdivisions) && (i < output.rows); i++) {
+          for (int j = y * ydivisions; (j < (y + 1) * ydivisions) && (j < output.cols); j++) {
+            if (output.at<float>(i, j) >= 220) {
+              int maxRadius = 0;
+              int temp = 0;
+              for (int r = 0; r < radius; r++) {
+                if (accumulator[i][j][r] > temp) {
+                  temp = accumulator[i][j][r];
+                  maxRadius = r;
+                }
+              }
+              circle(output, Point(j, i), maxRadius, Scalar(0, 0, 255), 2);
+              img.push_back(Rect((j-maxRadius) % output.cols, (i-maxRadius) % output.rows, 2*maxRadius % output.cols, 2*maxRadius % output.rows));
+              found = 1;
+              break;
+            }
+            if (found == 1) break;
           }
         }
-        circle(output, Point(j, i), maxRadius, Scalar(0, 0, 255), 2);
-        img.push_back(Rect((j-maxRadius) % output.cols, (i-maxRadius) % output.rows, 2*maxRadius % output.cols, 2*maxRadius % output.rows));
+        completed = 1;
       }
     }
   }
@@ -256,33 +264,30 @@ void detectAndDisplay( Mat input )
   Mat threshold = imread("threshold.jpg", 0);
 
   std::vector<Rect> imgDetected = hough(threshold, direction, 150);
+  // houghline(magnitude,direction);
 
   equalizeHist( gray_image, gray_image );
   std::vector<Rect> dartboards;
 
   cascade.detectMultiScale( gray_image, dartboards, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500, 500) );
 
-  // for( int i = 0; i < dartboards.size(); i++ )
-  // {
-  //   rectangle(input,dartboards[i], Scalar( 255, 0, 0 ), 2);
-  // }
-  // std::cout << dartboards.size() << std::endl;
+  //Viola-Jones
+  for( int i = 0; i < dartboards.size(); i++ )
+  {
+    rectangle(input,dartboards[i], Scalar( 255, 0, 0 ), 2);
+  }
+  std::cout << dartboards.size() << std::endl;
 
+  //Hough
   for( int k = 0; k < imgDetected.size(); k++ )
   {
     rectangle(input, imgDetected[k], Scalar( 0, 255, 0 ), 2);
   }
+  std::cout << imgDetected.size() << std::endl;
 
-	// for( int i = 0; i < img3.size(); i++ )
-	// {
-	// 	rectangle(input, img3[i], Scalar( 0, 0, 255 ), 2);
-	// }
-  float truePositives = 0;
-  float falsePositives = 0;
-  float falseNegatives = 0;
   std::vector<Rect> accurateViola;
   for (int i = 0; i < imgDetected.size(); i++) {
-		int correct = 0;
+    int found = 0;
 		for (int j = 0; j < dartboards.size(); j++) {
 			Rect r1(imgDetected[i].x, imgDetected[i].y,imgDetected[i].width,imgDetected[i].height);
 			Rect r2(dartboards[j].x, dartboards[j].y,dartboards[j].width,dartboards[j].height);
@@ -293,32 +298,49 @@ void detectAndDisplay( Mat input )
 			float uWidth = unionA.width;
 			float uHeight = unionA.height;
 			float areaRatio = (iWidth * iHeight) / (uWidth * uHeight);
-			if (areaRatio >= 0.5f) {
-				correct = 1;
-				truePositives += 1;
+			if (areaRatio >= 0.3f) {
         accurateViola.push_back(Rect(dartboards[j].x, dartboards[j].y,dartboards[j].width,dartboards[j].height));
-        rectangle(input,Point(dartboards[j].x, dartboards[j].y), Point(dartboards[j].x + dartboards[j].width, dartboards[j].y + dartboards[j].height), Scalar( 255, 0, 0 ), 2);
-				printf("IOU is %f\n",areaRatio);
+        found = 1;
+        break;
 			 }
+       if (found == 1) break;
 		}
-		if (correct == 0) {
-			falsePositives++;
-		}
-		falseNegatives = img3.size()-truePositives;
-	}
-	float recall = truePositives/(truePositives+falseNegatives);
-	float precision = truePositives/(truePositives+falsePositives);
-	float f1score = 2*((recall*precision)/(recall+precision));
-	printf("Tpr: %f\n", recall);
-	printf("True positives: %f\n", truePositives);
-	printf("False positives: %f\n", falsePositives);
-  printf("False negatives: %f\n", falseNegatives);
-	printf("F1 score is: %f\n", f1score);
-	imwrite("dart3Comb.jpg", input);
+
+    //Combined
+    for( int i = 0; i < accurateViola.size(); i++ )
+    {
+      rectangle(input, accurateViola[i], Scalar( 0, 0, 255 ), 2);
+    }
+    std::cout << accurateViola.size() << std::endl;
+	// 	if (correct == 0) {
+	// 		falsePositives++;
+	// 	}
+	// 	falseNegatives = img4.size()-truePositives;
+	// }
+	// float recall = truePositives/(truePositives+falseNegatives);
+	// float precision = truePositives/(truePositives+falsePositives);
+	// float f1score = 2*((recall*precision)/(recall+precision));
+	// printf("Tpr: %f\n", recall);
+	// printf("True positives: %f\n", truePositives);
+	// printf("False positives: %f\n", falsePositives);
+  // printf("False negatives: %f\n", falseNegatives);
+	// printf("F1 score is: %f\n", f1score);
+	imwrite("dart5Comb.jpg", input);
+
+  // float truePositives = 0;
+  // float falsePositives = 0;
+  // float falseNegatives = 0;
+  // for (int i = 0; i < accurateViola.size(); i++) {
+  //   int correct = 0;
+  //   for (int j = 0; j < img4.size(); j++) {
+  //
+  //   }
+  // }
+}
 }
 
 int main() {
-  Mat input = imread("dart3.jpg", 1);
+  Mat input = imread("dart5.jpg", 1);
   if( !cascade.load( cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
   detectAndDisplay(input);
   return 0;
